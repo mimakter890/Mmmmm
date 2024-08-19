b7bce31b732f9717d14c2d496590cc0ae6ab4699
ritu-mikail <mdmikail673@gmail.com>
2024-08-19 14:19:04 +0600

    Update Ab.js

--- scripts/commands/Ab.js
+++ scripts/commands/Ab.js
@@ -93,8 +93,8 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAvt1, 131, 106, 148, 172);
-  ctx.drawImage(baseAvt2, 444, 108, 154, 168);
+  ctx.drawImage(baseAvt1, 131, 104, 148, 172);
+  ctx.drawImage(baseAvt2, 444, 104, 148, 171);
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
   fs.removeSync(pathAvt1);
